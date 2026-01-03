0487eb88ec7c00c0d170db5ac44a29d51c1ef9ed
Roman Gurovich <romangurovich@gmail.com>
2026-01-03 00:42:29 -0800

    refactor(worker): use automatic workflow bundling for development

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,26 +1,10 @@
 import { Worker } from '@temporalio/worker';
 import * as activities from './activities';
-import { readFile } from 'fs/promises';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
 
 async function run() {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const bundlePath = join(__dirname, '../dist/workflow-bundle.js');
-  
-  let workflowBundle;
-  try {
-    const code = await readFile(bundlePath, 'utf8');
-    workflowBundle = { code };
-    console.log('Loaded workflow bundle from dist/workflow-bundle.js');
-  } catch (err) {
-    console.error('Failed to load workflow bundle. Make sure to run "bun run bundle" first.');
-    process.exit(1);
-  }
-
   // Step 1: Create a worker instance
   const worker = await Worker.create({
-    workflowBundle,
+    workflowsPath: require.resolve('./workflows'),
     activities,
     taskQueue: 'hn-jobs',
   });
