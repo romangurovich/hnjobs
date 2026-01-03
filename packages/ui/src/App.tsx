c9f3d52edc3f53462f5630a053b6f29e1e10c5cf
Roman Gurovich <romangurovich@gmail.com>
2026-01-03 02:22:12 -0800

    style(ui): fix sidebar layout typo

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       </header>
 
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col lg:row gap-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col lg:flex-row gap-8">
         {/* Sidebar */}
         <FilterPanel />
 
